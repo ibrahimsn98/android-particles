90405e70a30618314c89de9bca6ec03bdbcfc412
ibrahimsn98 <ibrahimsn98@gmail.com>
2018-12-25 12:01:11 +0000

    migrate from View to SurfaceView optimizations

--- app/src/main/java/me/ibrahimsn/particles/MainActivity.kt
+++ app/src/main/java/me/ibrahimsn/particles/MainActivity.kt
@@ -2,11 +2,26 @@ package me.ibrahimsn.particles
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import me.ibrahimsn.particle.ParticleView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var particleView: ParticleView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        particleView = findViewById(R.id.particleView)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        particleView.resume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        particleView.pause()
     }
 }
--- particle/src/main/java/me/ibrahimsn/particle/ParticleView.kt
+++ particle/src/main/java/me/ibrahimsn/particle/ParticleView.kt
@@ -1,23 +1,25 @@
 package me.ibrahimsn.particle
 
 import android.content.Context
-import android.graphics.Canvas
-import android.graphics.Color
-import android.graphics.Paint
-import android.graphics.Path
+import android.graphics.*
 import android.util.AttributeSet
-import android.view.View
+import android.view.SurfaceHolder
+import android.view.SurfaceView
 import android.view.ViewTreeObserver
 import kotlin.random.Random
 
-class ParticleView : View {
+class ParticleView : SurfaceView, SurfaceHolder.Callback {
 
-    private lateinit var particles: Array<Particle>
+    private lateinit var particles: Array<Particle?>
+
+    private var surfaceViewThread: SurfaceViewThread? = null
 
     private var count = 20
     private var minRadius = 5
     private var maxRadius = 10
+    private var hasSurface: Boolean = false
 
+    private var background = Color.BLACK
     private var color = Color.WHITE
     private val path = Path()
 
@@ -28,75 +30,144 @@ class ParticleView : View {
     }
 
     constructor(context: Context) : super(context)
-    constructor(context: Context, attrs: AttributeSet?) : super(context, attrs) {
+    constructor(context: Context, attrs: AttributeSet) : super(context, attrs) {
         val a = context.obtainStyledAttributes(attrs, R.styleable.ParticleView, 0, 0)
 
         count = a.getInt(R.styleable.ParticleView_particleCount, count)
         minRadius = a.getInt(R.styleable.ParticleView_minParticleRadius, minRadius)
         maxRadius = a.getInt(R.styleable.ParticleView_maxParticleRadius, maxRadius)
         color = a.getColor(R.styleable.ParticleView_particleColor, color)
+        background = a.getColor(R.styleable.ParticleView_backgroundColor, background)
+        a.recycle()
 
+        particles = arrayOfNulls(count)
         paint.color = color
 
-        if (count > 50)
-            count = 50
+        if (count > 50) count = 50
+        if (minRadius <= 0) minRadius = 1
+        if (maxRadius <= minRadius) maxRadius = minRadius + 1
 
-        if (minRadius <= 0)
-            minRadius = 1
+        if (holder != null)
+            holder.addCallback(this)
 
-        if (maxRadius <= minRadius)
-            maxRadius = minRadius + 1
-
-        a.recycle()
+        hasSurface = false
     }
 
-    init {
-        viewTreeObserver.addOnPreDrawListener(object: ViewTreeObserver.OnPreDrawListener {
-            override fun onPreDraw(): Boolean {
-                if (viewTreeObserver.isAlive)
-                    viewTreeObserver.removeOnPreDrawListener(this)
+    override fun surfaceCreated(holder: SurfaceHolder) {
+        hasSurface = true
 
-                val array = arrayOfNulls<Particle>(count)
+        if (surfaceViewThread == null)
+            surfaceViewThread = SurfaceViewThread()
 
-                for (i in 0 until count)
-                    array[i] = Particle(
-                        Random.nextInt(minRadius, maxRadius).toFloat(),
-                        Random.nextInt(0, width).toFloat(),
-                        Random.nextInt(0, height).toFloat(),
-                        Random.nextInt(-2, 2),
-                        Random.nextInt(-2, 2),
-                        Random.nextInt(150, 255))
-
-                particles =  array as Array<Particle>
-
-                return true
-            }
-        })
+        surfaceViewThread!!.start()
     }
 
-    override fun onDraw(canvas: Canvas) {
-        for (i in 0 until count) {
-            particles[i].x += particles[i].vx
-            particles[i].y += particles[i].vy
+    fun resume() {
+        if (surfaceViewThread == null) {
+            surfaceViewThread = SurfaceViewThread()
 
-            if (particles[i].x < 0)
-                particles[i].x = width.toFloat()
-            else if (particles[i].x > width)
-                particles[i].x = 0F
+            if (hasSurface)
+                surfaceViewThread!!.start()
+        }
+    }
 
-            if (particles[i].y < 0)
-                particles[i].y = height.toFloat()
-            else if (particles[i].y > height)
-                particles[i].y = 0F
+    fun pause() {
+        if (surfaceViewThread != null) {
+            surfaceViewThread!!.requestExitAndWait()
+            surfaceViewThread = null
+        }
+    }
 
-            for(j in 0 until count)
-                linkParticles(canvas, particles[i], particles[j])
+    override fun surfaceDestroyed(holder: SurfaceHolder) {
+        hasSurface = false
 
-            paint.alpha = particles[i].alpha
-            canvas.drawCircle(particles[i].x, particles[i].y, particles[i].radius, paint)
+        if (surfaceViewThread != null) {
+            surfaceViewThread!!.requestExitAndWait()
+            surfaceViewThread = null
+        }
+    }
+
+    override fun surfaceChanged(holder: SurfaceHolder, format: Int, w: Int, h: Int) {
+
+    }
+
+    private inner class SurfaceViewThread : Thread() {
+
+        private var running: Boolean = true
+
+        init {
+            running = true
+
+            viewTreeObserver.addOnPreDrawListener(object: ViewTreeObserver.OnPreDrawListener {
+                override fun onPreDraw(): Boolean {
+                    if (viewTreeObserver.isAlive)
+                        viewTreeObserver.removeOnPreDrawListener(this)
+
+                    for (i in 0 until count)
+                        particles[i] = Particle(
+                            Random.nextInt(minRadius, maxRadius).toFloat(),
+                            Random.nextInt(0, width).toFloat(),
+                            Random.nextInt(0, height).toFloat(),
+                            Random.nextInt(-2, 2),
+                            Random.nextInt(-2, 2),
+                            Random.nextInt(150, 255))
+
+                    return true
+                }
+            })
         }
 
-        postInvalidateDelayed(25)
+        override fun run() {
+            while (running) {
+                var canvas: Canvas? = null
+
+                try {
+                    canvas = holder.lockCanvas()
+
+                    synchronized (holder) {
+                        canvas.drawColor(background)
+
+                        for (i in 0 until count) {
+                            particles[i]!!.x += particles[i]!!.vx
+                            particles[i]!!.y += particles[i]!!.vy
+
+                            if (particles[i]!!.x < 0)
+                                particles[i]!!.x = width.toFloat()
+                            else if (particles[i]!!.x > width)
+                                particles[i]!!.x = 0F
+
+                            if (particles[i]!!.y < 0)
+                                particles[i]!!.y = height.toFloat()
+                            else if (particles[i]!!.y > height)
+                                particles[i]!!.y = 0F
+
+                            for (j in 0 until count)
+                                linkParticles(canvas, particles[i]!!, particles[j]!!)
+
+                            paint.alpha = particles[i]!!.alpha
+                            canvas.drawCircle(particles[i]!!.x, particles[i]!!.y, particles[i]!!.radius, paint)
+                        }
+                    }
+
+                } catch (e: Exception) {
+                    e.printStackTrace()
+                } finally {
+                    if (canvas != null) {
+                        holder.unlockCanvasAndPost(canvas)
+                    }
+                }
+            }
+        }
+
+        fun requestExitAndWait() {
+            running = false
+
+            try {
+                join()
+            } catch (ignored: InterruptedException) {
+
+            }
+        }
     }
 
     private fun linkParticles(canvas: Canvas, p1: Particle, p2: Particle) {
