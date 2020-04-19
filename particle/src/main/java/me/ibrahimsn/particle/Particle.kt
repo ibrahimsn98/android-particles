ed503c324fd2f6c160d926a352c3bda3298ac076
melianor <ibrahimsn98@gmail.com>
2020-04-19 14:45:57 +0300

    performance optimizations, refactoring

---
+++ particle/src/main/java/me/ibrahimsn/particle/Particle.kt
@@ -0,0 +1,10 @@
+package me.ibrahimsn.particle
+
+data class Particle constructor(
+    var radius: Float,
+    var x: Float,
+    var y: Float,
+    var vx: Int,
+    var vy: Int,
+    var alpha: Int
+)
--- particle/src/main/java/me/ibrahimsn/particle/ParticleView.kt
+++ particle/src/main/java/me/ibrahimsn/particle/ParticleView.kt
@@ -1,10 +1,14 @@
 package me.ibrahimsn.particle
 
 import android.content.Context
-import android.graphics.*
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
+import android.graphics.Path
 import android.util.AttributeSet
 import android.view.SurfaceHolder
 import android.view.SurfaceView
+import kotlin.math.min
 import kotlin.math.sqrt
 import kotlin.random.Random
 
@@ -16,7 +20,7 @@ class ParticleView : SurfaceView, SurfaceHolder.Callback {
     private var count = 20
     private var minRadius = 5
     private var maxRadius = 10
-    private var lines = true
+    private var isLinesEnabled = true
     private var hasSurface: Boolean = false
 
     private var background = Color.BLACK
@@ -26,13 +30,13 @@ class ParticleView : SurfaceView, SurfaceHolder.Callback {
 
     private val paintParticles: Paint = Paint().apply {
         isAntiAlias = true
-        style = Paint.Style.FILL_AND_STROKE
+        style = Paint.Style.FILL
         strokeWidth = 2F
     }
 
     private val paintLines: Paint = Paint().apply {
         isAntiAlias = true
-        style = Paint.Style.FILL_AND_STROKE
+        style = Paint.Style.STROKE
         strokeWidth = 2F
     }
 
@@ -40,7 +44,7 @@ class ParticleView : SurfaceView, SurfaceHolder.Callback {
     constructor(context: Context, attrs: AttributeSet) : super(context, attrs) {
         val a = context.obtainStyledAttributes(attrs, R.styleable.ParticleView, 0, 0)
 
-        lines = a.getBoolean(R.styleable.ParticleView_lines, lines)
+        isLinesEnabled = a.getBoolean(R.styleable.ParticleView_lines, isLinesEnabled)
         count = a.getInt(R.styleable.ParticleView_particleCount, count)
         minRadius = a.getInt(R.styleable.ParticleView_minParticleRadius, minRadius)
         maxRadius = a.getInt(R.styleable.ParticleView_maxParticleRadius, maxRadius)
@@ -56,8 +60,9 @@ class ParticleView : SurfaceView, SurfaceHolder.Callback {
         if (minRadius <= 0) minRadius = 1
         if (maxRadius <= minRadius) maxRadius = minRadius + 1
 
-        if (holder != null)
+        if (holder != null) {
             holder.addCallback(this)
+        }
 
         hasSurface = false
     }
@@ -65,26 +70,26 @@ class ParticleView : SurfaceView, SurfaceHolder.Callback {
     override fun surfaceCreated(holder: SurfaceHolder) {
         hasSurface = true
 
-        if (surfaceViewThread == null)
+        if (surfaceViewThread == null) {
             surfaceViewThread = SurfaceViewThread()
+        }
 
-        surfaceViewThread!!.start()
+        surfaceViewThread?.start()
     }
 
     fun resume() {
         if (surfaceViewThread == null) {
             surfaceViewThread = SurfaceViewThread()
 
-            if (hasSurface)
-                surfaceViewThread!!.start()
+            if (hasSurface) {
+                surfaceViewThread?.start()
+            }
         }
     }
 
     fun pause() {
-        if (surfaceViewThread != null) {
-            surfaceViewThread!!.requestExitAndWait()
-            surfaceViewThread = null
-        }
+        surfaceViewThread?.requestExitAndWait()
+        surfaceViewThread = null
     }
 
     override fun surfaceDestroyed(holder: SurfaceHolder) {
@@ -97,59 +102,68 @@ class ParticleView : SurfaceView, SurfaceHolder.Callback {
     }
 
     override fun surfaceChanged(holder: SurfaceHolder, format: Int, w: Int, h: Int) {
-
+        // Ignore
     }
 
     override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
         super.onSizeChanged(w, h, oldw, oldh)
-        if (particles.size == 0)
-            for (i in 0 until count)
-                particles.add(Particle(Random.nextInt(minRadius, maxRadius).toFloat(),
-                    Random.nextInt(0, width).toFloat(),
-                    Random.nextInt(0, height).toFloat(),
-                    Random.nextInt(-2, 2),
-                    Random.nextInt(-2, 2),
-                    Random.nextInt(150, 255)))
+        if (particles.size == 0) {
+            for (i in 0 until count) {
+                particles.add(
+                    Particle(
+                        Random.nextInt(minRadius, maxRadius).toFloat(),
+                        Random.nextInt(0, width).toFloat(),
+                        Random.nextInt(0, height).toFloat(),
+                        Random.nextInt(-2, 2),
+                        Random.nextInt(-2, 2),
+                        Random.nextInt(150, 255)
+                    )
+                )
+            }
+        }
     }
 
     private inner class SurfaceViewThread : Thread() {
 
-        private var running: Boolean = true
-
-        init {
-            running = true
-        }
+        private var running = true
+        private var canvas: Canvas? = null
 
         override fun run() {
             while (running) {
-                var canvas: Canvas? = null
-
                 try {
                     canvas = holder.lockCanvas()
 
                     synchronized (holder) {
-                        canvas.drawColor(background)
+                        canvas?.drawColor(background)
 
                         for (i in 0 until count) {
                             particles[i].x += particles[i].vx
                             particles[i].y += particles[i].vy
 
-                            if (particles[i].x < 0)
+                            if (particles[i].x < 0) {
                                 particles[i].x = width.toFloat()
-                            else if (particles[i].x > width)
+                            } else if (particles[i].x > width) {
                                 particles[i].x = 0F
+                            }
 
-                            if (particles[i].y < 0)
+                            if (particles[i].y < 0) {
                                 particles[i].y = height.toFloat()
-                            else if (particles[i].y > height)
+                            } else if (particles[i].y > height) {
                                 particles[i].y = 0F
+                            }
 
-                            if (lines)
-                                for (j in 0 until count)
-                                    linkParticles(canvas, particles[i], particles[j])
+                            canvas?.let {
+                                if (isLinesEnabled) {
+                                    for (j in 0 until count) {
+                                        if (i != j) {
+                                            linkParticles(it, particles[i], particles[j])
+                                        }
+                                    }
+                                }
+                            }
 
                             paintParticles.alpha = particles[i].alpha
-                            canvas.drawCircle(particles[i].x, particles[i].y, particles[i].radius, paintParticles)
+                            canvas?.drawCircle(particles[i].x, particles[i].y, particles[i].radius, paintParticles)
                         }
                     }
 
@@ -174,22 +188,25 @@ class ParticleView : SurfaceView, SurfaceHolder.Callback {
         }
     }
 
-    private fun linkParticles(canvas: Canvas, p1: Particle, p2: Particle) {
-        val dx = p1.x - p2.x
-        val dy = p1.y - p2.y
-        val dist = sqrt((dx * dx + dy * dy).toDouble()).toInt()
+    private var dx: Float = 0f
+    private var dy: Float = 0f
+    private var dist: Float = 0f
+    private var distRatio: Float = 0f
 
-        if (dist < 225) {
+    private fun linkParticles(canvas: Canvas, p1: Particle, p2: Particle) {
+        dx = p1.x - p2.x
+        dy = p1.y - p2.y
+        dist = sqrt(dx * dx + dy * dy)
+
+        if (dist < 220) {
             path.moveTo(p1.x, p1.y)
             path.lineTo(p2.x, p2.y)
+            distRatio = (220 - dist) / 220
 
-            paintLines.alpha = 250 - dist
+            paintLines.alpha = (min(p1.alpha, p2.alpha) * distRatio / 2).toInt()
             canvas.drawPath(path, paintLines)
 
             path.reset()
         }
     }
-
-    data class Particle constructor(var radius: Float, var x: Float, var y: Float,
-                                    var vx: Int, var vy: Int, var alpha: Int)
 }
